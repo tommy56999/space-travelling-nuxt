478d7c079171fc370dd712e53764425785c788c5
tommy56999 <peterl.tommy@icloud.com>
2022-03-07 12:02:00 +0100

    2 files modified

--- pages/destination.vue
+++ pages/destination.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1 class="numbered-title"><span>01</span> Pick your destination</h1>
+  <div>
+    <h1 class="numbered-title"><span>01</span> Pick your destination</h1>
+
+    <div id="content" v-if="destination">
+      <img :src="destination['png-image'].guid" alt="Mond-Foto" />
+
+      <ul>
+        <li
+          v-for="(item, index) in destinations"
+          :key="item['destination-name']"
+          @click="setDestination(index)"
+        >
+          {{ item['destination-name'] }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +23,11 @@ export default {
   data() {
     return {
       destinations: [],
+
+      activeDestinationIndex: 1,
     }
   },
+
   methods: {
     async loadData() {
       const response = await fetch(
@@ -17,15 +36,27 @@ export default {
 
       this.destinations = await response.json()
     },
+
+    setDestination(index) {
+      this.activeDestinationIndex = index
+    },
   },
+
   mounted() {
     this.loadData()
 
     document.body.style.backgroundImage =
       "url('background-destination-mobile.jpg')"
   },
+
+  computed: {
+    destination() {
+      return this.destinations[this.activeDestinationIndex]
+    },
+  },
 }
 </script>
+
 <style scoped>
 .numbered-title {
   font-size: 16px;
