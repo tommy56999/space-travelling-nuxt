56b45e57a8a50f274b530bc570a6c1f5c86855e0
tommy56999 <peterl.tommy@icloud.com>
2022-03-17 19:26:04 +0100

    2 files modified

--- pages/destination.vue
+++ pages/destination.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <h1 class="numbered-title"><span>01</span> Pick your destination</h1>
-
-    <div id="content" v-if="destination">
-      <img :src="destination['png-image'].guid" alt="Mond-Foto" />
-
-      <ul>
-        <li
-          v-for="(item, index) in destinations"
-          :key="item['destination-name']"
-          @click="setDestination(index)"
-        >
-          {{ item['destination-name'] }}
-        </li>
-      </ul>
+    <div class="container flow flex crew">
+      <h1 class="numbered-title"><span>01</span> Pick your destination</h1>
+      <div id="content" v-if="destination">
+        <img :src="destination['png-image'].guid" alt="Mond-Foto" />
+        <ul>
+          <li
+            v-for="(item, index) in destinations"
+            :key="item['destination-name']"
+            @click="setDestination(index)"
+          >
+            {{ item['destination-name'] }}
+          </li>
+        </ul>
+        <h1>{{ destination['destination-name'] }}</h1>
+        <p>{{ destination.description }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -23,32 +25,25 @@ export default {
   data() {
     return {
       destinations: [],
-
       activeDestinationIndex: 1,
     }
   },
-
   methods: {
     async loadData() {
       const response = await fetch(
         'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/destination'
       )
-
       this.destinations = await response.json()
     },
-
     setDestination(index) {
       this.activeDestinationIndex = index
     },
   },
-
   mounted() {
     this.loadData()
-
     document.body.style.backgroundImage =
       "url('background-destination-mobile.jpg')"
   },
-
   computed: {
     destination() {
       return this.destinations[this.activeDestinationIndex]
